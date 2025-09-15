b7a66da0f48b5f299d78233de302d5918a454fb0
mariomafdyhabib <mariomafdy333@gmail.com>
2025-09-15 16:21:43 +0300

    terraform edit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,11 +12,3 @@ variable "instance_type" {
 
 }
 
-resource "aws_vpc" "ci_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "ci_subnet" {
-  vpc_id     = aws_vpc.ci_vpc.id
-  cidr_block = "10.0.1.0/24"
-}
