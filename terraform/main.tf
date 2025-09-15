9e0cd3bcf57dde9cfb37819af9dde96a313beea0
mariomafdyhabib <mariomafdy333@gmail.com>
2025-09-15 16:15:49 +0300

    sg edit

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,3 +45,56 @@ resource "aws_ssm_parameter" "ci_ephemeral_ip" {
   type  = "String"
   value = aws_instance.ci_ephemeral.public_ip
 }
+
+resource "aws_internet_gateway" "ci_igw" {
+  vpc_id = aws_vpc.ci_vpc.id
+}
+
+resource "aws_route_table" "ci_route_table" {
+  vpc_id = aws_vpc.ci_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ci_igw.id
+  }
+}
+
+resource "aws_route_table_association" "ci_subnet_assoc" {
+  subnet_id      = aws_subnet.ci_subnet.id
+  route_table_id = aws_route_table.ci_route_table.id
+}
+
+resource "aws_security_group" "ci_sg" {
+  name        = "ci_sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.ci_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # or your Jenkins/public IP only
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ci_ephemeral" {
+  ami                         = "ami-0b09ffb6d8b58ca91"
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  key_name                    = "Jenkins"
+  subnet_id                   = aws_subnet.ci_subnet.id
+  vpc_security_group_ids      = [aws_security_group.ci_sg.id]
+
+  tags = {
+    Name     = "ci-ephemeral"
+    lifespan = "ephemeral"
+    owner    = "jenkins"
+  }
+}
